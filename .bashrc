2a8113577a0dd9be006643e60a7cb787c3791dd6
Vladimir Kravtsov <vladhc@gmail.com>
2022-08-25 18:10:53 +0200

    Added Sway configs

--- .bashrc
+++ .bashrc
@@ -117,8 +117,6 @@ if ! shopt -oq posix; then
   fi
 fi
 
-export PATH="$HOME/.local/bin:$PATH"
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
---
+++ .config/sway/i3exit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case "$1" in
+  logout)
+    swaymsg exit
+    ;;
+  suspend)
+    systemctl suspend
+    ;;
+  hibernate)
+    systemctl hibernate
+    ;;
+  reboot)
+    systemctl reboot
+    ;;
+  shutdown)
+    systemctl poweroff
+    ;;
+  *)
+    echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+    exit 2
+esac
+
+exit 0
